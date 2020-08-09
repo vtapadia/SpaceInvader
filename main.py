b7c6fc83ee49986e76cd842e13317441f2777421
Varesh Tapadia <varesh.tapadia@rabobank.nl>
2020-08-09 13:05:23 +0200

    after collision setup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -11,23 +12,61 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load("spaceship.png")
 pygame.display.set_icon(icon)
 
-playerImg = pygame.image.load("space-invaders.png")
+playerImg = pygame.image.load("player.png")
 playerX = 370
-playerY = 470
+playerY = 480
 playerX_change = 0
 playerY_change = 0
 
-enemyImg = pygame.image.load("spaceship.png")
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 4
-enemyY_change = 30
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletY_change = 10
+# Ready => "
+bullet_state = "ready"
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load("enemy.png"))
+    enemyX.append(random.randint(0, 800))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4)
+    enemyY_change.append(20)
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+textX = 10
+textY = 10
+
+
+def show_score(x, y):
+    score = font.render("Score :" + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 16))
+
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + math.pow(enemyY - bulletY, 2))
+    return distance < 27
+
 
 def xBound(x):
     if x <= 0:
@@ -35,6 +74,7 @@ def xBound(x):
     elif x >= 760:
         x = 760
     return x
+
 
 running = True
 while running:
@@ -48,9 +88,21 @@ while running:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
+            if event.key == pygame.K_SPACE and bullet_state == "ready":
+                bulletX = playerX
+                fire_bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
                 playerX_change = 0
+
+    # Bullet movement
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
 
     playerX += playerX_change
     # Checking for boundaries
@@ -59,16 +111,24 @@ while running:
     elif playerX >= 760:
         playerX = 760
 
-    if enemyX <= 0:
-        enemyX_change = 4
-        enemyY += enemyY_change
-    elif enemyX >= 760:
-        enemyX_change = -4
-        enemyY += enemyY_change
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 760:
+            enemyX_change[i] = -4
+            enemyY[i] += enemyY_change[i]
 
-    enemyX += enemyX_change
-    # playerY += playerY_change
-    enemy(enemyX, enemyY)
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bullet_state = "ready"
+            bulletY = 480
+            score_value += 1
+            enemyX[i] = random.randint(0, 800)
+            enemyY[i] = random.randint(50, 150)
+        enemy(enemyX[i], enemyY[i], i)
+
     player(playerX, playerY)
+    show_score(textX, textY)
     pygame.display.update()
-
